011737e82a8019aa943e218805cdc817525d00b8
Felipe (Home) <sharpion.k@gmail.com>
2022-07-13 20:50:09 +0100

    Lint

--- src/sections/home/Home.test.tsx
+++ src/sections/home/Home.test.tsx
@@ -3,10 +3,10 @@ import { render } from '@testing-library/react';
 import { Home } from './Home';
 
 describe('Home', () => {
-  const renderComponent = ({}) => render(<Home />);
+  const renderComponent = () => render(<Home />);
 
   it('should render', () => {
-    const { container } = renderComponent({});
+    const { container } = renderComponent();
     expect(container).toMatchSnapshot();
   });
 });
