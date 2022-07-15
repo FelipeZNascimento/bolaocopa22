8e86cec51816018125e73a4098b164dd27473b7e
Felipe (Home) <sharpion.k@gmail.com>
2022-07-15 19:40:57 +0100

    Updating with new Match props

--- src/constants/mocks.tsx
+++ src/constants/mocks.tsx
@@ -1,28 +1,62 @@
-import { IMatchProps, TTeam } from '@omegafox/components';
+import React from 'react';
+import {
+  IMatchProps,
+  ITeamProps,
+  TClockFootball,
+  FOOTBALL_MATCH_STATUS
+} from '@omegafox/components';
 
-export const teamLeft: TTeam = {
+export const teamLeft: ITeamProps = {
   id: 0,
   align: 'left',
-  colors: ['#FFDC02', '#193375'],
+  colors: ['#FFDC02', '#19AE47'],
+  isEditable: false,
   logo: 'https://teamcolorcodes.com/wp-content/uploads/2021/12/Brazil-National-Football-Team-Logo-211x300.png',
   name: 'Brasil',
   nameShort: 'BRA',
   score: 4
 };
 
-export const teamRight: TTeam = {
+export const teamRight: ITeamProps = {
   id: 1,
   align: 'right',
   colors: ['#FFFFFF', '#43A1D5'],
+  isEditable: false,
   logo: 'https://teamcolorcodes.com/wp-content/uploads/2021/12/Argentina-National-Football-Team-Logo-192x300.png',
   name: 'Argentina',
   nameShort: 'ARG',
   score: 0
 };
 
+const teamLeftNoBet = {
+  ...teamLeft,
+  score: null
+};
+
+const teamRightNoBet = {
+  ...teamRight,
+  score: null
+};
+
+export const footballClock: TClockFootball = {
+  time: 45,
+  status: FOOTBALL_MATCH_STATUS.FIRST_HALF
+};
+
 export const matchInfo: IMatchProps = {
+  expandableContent: () => {
+    return <p>Vila Capanema</p>;
+  },
+  clock: footballClock,
   timestamp: 1662682800,
-  location: 'Curitiba, Paraná',
-  stadium: 'Vila Capanema',
+  sport: 'football',
   teams: [teamLeft, teamRight]
+};
+
+export const matchBetInfo: IMatchProps = {
+  ...matchInfo,
+  clock: footballClock,
+  timestamp: 1662682800,
+  sport: 'football',
+  teams: [teamLeftNoBet, teamRightNoBet]
 };
--- src/sections/Results/Results.tsx
+++ src/sections/Results/Results.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { isMobile } from 'react-device-detect';
 import classNames from 'classnames';
 
-import { Match } from '@omegafox/components';
+import { Match, FOOTBALL_MATCH_STATUS } from '@omegafox/components';
 import { matchInfo } from 'constants/mocks';
 import styles from './Results.module.scss';
 
@@ -22,80 +22,147 @@ export const Results = () => {
       <div className={leftSectionClass}>
         <div className={styles.match}>
           <Match
-            location={matchInfo.location}
-            stadium={matchInfo.stadium}
+            isExpandable
+            isEditable={false}
+            expandableContent={() => {
+              return <p>Vila Capanema</p>;
+            }}
+            clock={{ time: 0, status: FOOTBALL_MATCH_STATUS.NOT_STARTED }}
             timestamp={matchInfo.timestamp}
             teams={matchInfo.teams}
           />
         </div>
         <div className={styles.match}>
           <Match
-            location={matchInfo.location}
-            stadium={matchInfo.stadium}
+            isExpandable
+            isEditable={false}
+            expandableContent={() => {
+              return <p>Vila Capanema</p>;
+            }}
+            clock={{ time: 39, status: FOOTBALL_MATCH_STATUS.FIRST_HALF }}
             timestamp={matchInfo.timestamp}
             teams={matchInfo.teams}
           />
         </div>
         <div className={styles.match}>
           <Match
-            location={matchInfo.location}
-            stadium={matchInfo.stadium}
+            isExpandable
+            isEditable={false}
+            expandableContent={() => {
+              return <p>Vila Capanema</p>;
+            }}
+            clock={{ time: 39, status: FOOTBALL_MATCH_STATUS.HALFTIME }}
             timestamp={matchInfo.timestamp}
             teams={matchInfo.teams}
           />
         </div>
         <div className={styles.match}>
           <Match
-            location={matchInfo.location}
-            stadium={matchInfo.stadium}
+            isExpandable
+            isEditable={false}
+            expandableContent={() => {
+              return <p>Vila Capanema</p>;
+            }}
+            clock={{ time: 46, status: FOOTBALL_MATCH_STATUS.SECOND_HALF }}
             timestamp={matchInfo.timestamp}
             teams={matchInfo.teams}
           />
         </div>
         <div className={styles.match}>
           <Match
-            location={matchInfo.location}
-            stadium={matchInfo.stadium}
+            isExpandable
+            isEditable={false}
+            expandableContent={() => {
+              return <p>Vila Capanema</p>;
+            }}
+            clock={{
+              time: 46,
+              status: FOOTBALL_MATCH_STATUS.AWAITING_OVERTIME
+            }}
             timestamp={matchInfo.timestamp}
             teams={matchInfo.teams}
           />
         </div>
         <div className={styles.match}>
           <Match
-            location={matchInfo.location}
-            stadium={matchInfo.stadium}
+            isExpandable
+            isEditable={false}
+            expandableContent={() => {
+              return <p>Vila Capanema</p>;
+            }}
+            clock={{
+              time: 13,
+              status: FOOTBALL_MATCH_STATUS.FIRST_HALF_OVERTIME
+            }}
             timestamp={matchInfo.timestamp}
             teams={matchInfo.teams}
           />
         </div>
         <div className={styles.match}>
           <Match
-            location={matchInfo.location}
-            stadium={matchInfo.stadium}
+            isExpandable
+            isEditable={false}
+            expandableContent={() => {
+              return <p>Vila Capanema</p>;
+            }}
+            clock={{
+              time: 46,
+              status: FOOTBALL_MATCH_STATUS.HALFTIME_OVERTIME
+            }}
             timestamp={matchInfo.timestamp}
             teams={matchInfo.teams}
           />
         </div>
         <div className={styles.match}>
           <Match
-            location={matchInfo.location}
-            stadium={matchInfo.stadium}
+            isExpandable
+            isEditable={false}
+            expandableContent={() => {
+              return <p>Vila Capanema</p>;
+            }}
+            clock={{
+              time: 15,
+              status: FOOTBALL_MATCH_STATUS.SECOND_HALF_OVERTIME
+            }}
             timestamp={matchInfo.timestamp}
             teams={matchInfo.teams}
           />
         </div>
         <div className={styles.match}>
           <Match
-            location={matchInfo.location}
-            stadium={matchInfo.stadium}
+            isExpandable
+            isEditable={false}
+            expandableContent={() => {
+              return <p>Vila Capanema</p>;
+            }}
+            clock={{ time: 46, status: FOOTBALL_MATCH_STATUS.FINAL_OVERTIME }}
             timestamp={matchInfo.timestamp}
             teams={matchInfo.teams}
           />
         </div>
         <div className={styles.match}>
           <Match
-            location={matchInfo.location}
-            stadium={matchInfo.stadium}
+            isExpandable
+            isEditable={false}
+            expandableContent={() => {
+              return <p>Vila Capanema</p>;
+            }}
+            clock={{
+              time: 46,
+              status: FOOTBALL_MATCH_STATUS.AWAITING_PENALTIES
+            }}
+            timestamp={matchInfo.timestamp}
+            teams={matchInfo.teams}
+          />
+        </div>
+        <div className={styles.match}>
+          <Match
+            isExpandable
+            isEditable={false}
+            expandableContent={() => {
+              return <p>Vila Capanema</p>;
+            }}
+            clock={{ time: 0, status: FOOTBALL_MATCH_STATUS.PENALTIES }}
             timestamp={matchInfo.timestamp}
             teams={matchInfo.teams}
           />
