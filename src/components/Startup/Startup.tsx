d064b124fba6bf9d06daadbfd1500cda206691ac
Felipe (Home) <sharpion.k@gmail.com>
2022-09-24 17:40:00 +0100

    Matches no longer loading on startup. Only @Results.

--- src/components/Startup/Startup.tsx
+++ src/components/Startup/Startup.tsx
@@ -4,7 +4,6 @@ import { useDispatch } from 'react-redux';
 // Store
 import { useGetConfigQuery } from 'store/base/base';
 import { userLoggedIn, userLoginLoading } from 'store/user/reducer';
-import { matchesLoading, matchesSet } from 'store/match/reducer';
 import { IStartup } from './types';
 import { QueryHandler } from 'services/queryHandler';
 import { teamsSet } from 'store/team/reducer';
@@ -15,13 +14,11 @@ export const Startup = ({ children }: IStartup) => {
 
   useEffect(() => {
     dispatch(userLoginLoading(isLoading));
-    dispatch(matchesLoading(isLoading));
 
     if (!error && !isLoading && data) {
       const result = QueryHandler(data);
       if (result) {
         dispatch(userLoggedIn(result.loggedUser));
-        dispatch(matchesSet(result.matches));
         dispatch(teamsSet(result.teams));
       }
     }
--- src/store/match/actions.ts
+++ src/store/match/actions.ts
@@ -2,13 +2,19 @@ import { baseApi } from 'store/base/base';
 // import { TMatch } from './types';
 
 import {
-  updateBet as updateBetEndpoint,
-  listAllMatchesWithUserBets as listAllMatchesWithUserBetsEndpoint
+  listAllMatchesWithUserBets as listAllMatchesWithUserBetsEndpoint,
+  listAllMatches as listAllMatchesEndpoint
 } from 'services/endpoints';
 import { TQuery } from 'store/base/types';
 
 const extendedApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
+    onListAllMatches: builder.query<TQuery, null>({
+      query: () => {
+        return { url: listAllMatchesEndpoint(), credentials: 'include' };
+      }
+    }),
+
     onListAllMatchesWithUserBets: builder.mutation<TQuery, void>({
       query: () => {
         return {
@@ -20,40 +26,11 @@ const extendedApi = baseApi.injectEndpoints({
           credentials: 'include'
         };
       }
-    }),
-    onUpdateBet: builder.mutation<
-      TQuery,
-      {
-        betId: number | null;
-        matchId: number;
-        userId: number;
-        goalsHome: number | null;
-        goalsAway: number | null;
-      }
-    >({
-      query: (arg) => {
-        const { betId, matchId, userId, goalsHome, goalsAway } = arg;
-        return {
-          url: updateBetEndpoint(),
-          method: 'post',
-          body: {
-            id: betId,
-            idMatch: matchId,
-            idUser: userId,
-            goalsHome: goalsHome,
-            goalsAway: goalsAway
-          },
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-          },
-          credentials: 'include'
-        };
-      }
     })
   })
 });
 
 export const {
-  useOnUpdateBetMutation,
-  useOnListAllMatchesWithUserBetsMutation
+  useOnListAllMatchesWithUserBetsMutation,
+  useOnListAllMatchesQuery
 } = extendedApi;
